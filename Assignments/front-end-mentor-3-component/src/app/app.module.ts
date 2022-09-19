5dfa24e7e77438c53767a4a132611afdbfb20a7b
Nevaden <aaron.k.dodson@gmail.com>
2022-09-19 16:31:07 -0400

    late commit, cards mostly completed.

--- Assignments/front-end-mentor-3-component/src/app/app.module.ts
+++ Assignments/front-end-mentor-3-component/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SedansComponent } from './sedans/sedans.component';
+import { SuvsComponent } from './suvs/suvs.component';
+import { LuxuryComponent } from './luxury/luxury.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SedansComponent,
+    SuvsComponent,
+    LuxuryComponent
   ],
   imports: [
     BrowserModule
