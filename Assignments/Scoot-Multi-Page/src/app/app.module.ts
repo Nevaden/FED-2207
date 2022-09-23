7fea349ad666075d9265a1402d0ef8fe11188107
Nevaden <aaron.k.dodson@gmail.com>
2022-09-23 10:57:58 -0400

    created main components

--- Assignments/Scoot-Multi-Page/src/app/app.module.ts
+++ Assignments/Scoot-Multi-Page/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { CareersComponent } from './Components/careers/careers.component';
+import { LocationsComponent } from './Components/locations/locations.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CareersComponent,
+    LocationsComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
