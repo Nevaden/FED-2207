6671d457ea2b6d1ce33edb0637a5cd88bef1a5a0
Nevaden <aaron.k.dodson@gmail.com>
2022-09-28 00:00:49 -0400

    about completed except for accordion.

--- Assignments/Scoot-Multi-Page/src/app/Components/careers/careers.component.ts
+++ Assignments/Scoot-Multi-Page/src/app/Components/careers/careers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-careers',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./careers.component.css']
 })
 export class CareersComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
