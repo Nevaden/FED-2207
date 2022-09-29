d2881448d22f1c9910fc2225e5f6c3b50bcff9de
Nevaden <aaron.k.dodson@gmail.com>
2022-09-29 18:58:53 -0400

    fully complete. woohooo(accordion all sizes)

--- Assignments/Scoot-Multi-Page/src/app/Components/about/faq/accordion/accordion.component.ts
+++ Assignments/Scoot-Multi-Page/src/app/Components/about/faq/accordion/accordion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { accordianFAQS } from 'src/app/Data/accordionFAQS';
 
 @Component({
   selector: 'app-accordion',
@@ -6,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent implements OnInit {
-  items = ['Item 1',"chevron encoded"];
+  // items = ['How do I download the app?',"Can I find a nearby Scoots?","Do I need a license to ride?"];
+  AccordionArticles = accordianFAQS;
   expandedIndex = 0;
   cheveronLocked = '';
   constructor() { }
 
   ngOnInit(): void {
-    
+    console.log(this.AccordionArticles, "full object")
+    console.log(this.AccordionArticles[0],"1st")
+    console.log(this.AccordionArticles[1],"2nd")
   }
 
 
